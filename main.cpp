45dfe0e8d0d618b8e8006b15560dfb49956fd0e9
FaisalKhan444 <146987029+FaisalKhan444@users.noreply.github.com>
2025-01-30 02:21:58 +0500

    added a function of sum

--- main.cpp
+++ main.cpp
@@ -2,5 +2,11 @@
 // Created by Faisal Khan on 5/22/2024.
 #include <iostream>
 using namespace std;
+void fun(int x, int y){
+    cout<<"The sum is ="<<  x+y;
+}
 int main(){
-    cout <<"Hello World";}
+    cout <<"Hello World";
+    fun(5,10);
+    return 0;
+}
